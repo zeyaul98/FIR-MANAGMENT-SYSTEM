7039d8c6c23bf418a759fcbe32fb7b90f2acfe83
zeyaul98 <zeyaul445@gmail.com>
2026-05-29 23:06:59 +0530

    vite correct

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,10 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // server: {
-  //    host: "0.0.0.0",
-  //   allowedHosts: [
-  //     ".ngrok-free.app",
-  //   ],
-  // },
 })
